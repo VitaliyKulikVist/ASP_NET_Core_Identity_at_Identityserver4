47ffbdd132698d39bc9603e65a337ea25be69214
VitalikKulik <vitalikkylik@gmail.com>
2022-12-02 17:45:08 +0300

    Добавьте файлы проекта.

---
+++ ASP_NET_Core_Identity_at_Identityserver4/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ASP_NET_Core_Identity_at_Identityserver4.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
